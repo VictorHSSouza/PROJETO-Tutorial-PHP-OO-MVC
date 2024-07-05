681c23bb02f709cec3c82b779f1870a6f9cb4e62
VictorHSSouza <118738436+VictorHSSouza@users.noreply.github.com>
2024-07-05 09:14:11 -0300

    Update primeirocrud.sql

--- primeirocrud.sql
+++ primeirocrud.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 24/06/2024 às 08:04
+-- Tempo de geração: 05/07/2024 às 09:12
 -- Versão do servidor: 8.2.0
 -- Versão do PHP: 8.2.13
 
@@ -20,8 +20,7 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `primeirocrud`
 --
-create database primeirocrud;
-use primeirocrud;
+
 -- --------------------------------------------------------
 
 --
@@ -31,21 +30,21 @@ use primeirocrud;
 DROP TABLE IF EXISTS `client`;
 CREATE TABLE IF NOT EXISTS `client` (
   `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
-  `nome` varchar(30) NOT NULL,
+  `name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `email` varchar(50) NOT NULL,
   `cpf` char(14) NOT NULL,
-  `nascimento` date NOT NULL,
-  `telefone` varchar(14) NOT NULL,
-  `endereco` varchar(255) NOT NULL,
+  `birth` date NOT NULL,
+  `address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `phone` char(14) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Despejando dados para a tabela `client`
 --
 
-INSERT INTO `client` (`id`, `nome`, `email`, `cpf`, `nascimento`, `telefone`, `endereco`) VALUES
-(1, 'henricão', 'henricão@gmail.com', '11111111111111', '2024-06-18', '11111111111111', 'dfhi5uht6t');
+INSERT INTO `client` (`id`, `name`, `email`, `cpf`, `birth`, `address`, `phone`) VALUES
+(2, 'vicpocjcps', 'cbisaibcai@edu.com', '22222222222', '2024-06-09', '444444444', 'vsfdfsdfsdfds');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
